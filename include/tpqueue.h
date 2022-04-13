7e85f7a2d3f8a009e0392ac0fd81f94eb609da46
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-04-13 22:36:12 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -13,18 +13,18 @@ class TPQueue {
   TPQueue() : begin(0), end(0) {}
   void push(T element) {
     int i = end;
-    for (int i; i > begin; i--) {
+    for (i; i >= begin; i--) {
       if (element.prior > arr[i % size].prior) {
-        arr[i % size] = arr[(i + 1) % size];
+        arr[i % size] = arr[(i - 1) % size];
       } else {
         break;
       }
     }
-    arr[i] = element;
+    arr[i + 1] = element;
     end++;
   }
   T& pop() {
-    return arr[++begin % size];
+    return arr[(begin++) % size];
   }
 };
 
