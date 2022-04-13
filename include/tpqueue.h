d16052dc63aaca3e0c49ac08d637b292711d38fe
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-04-13 22:42:31 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,28 +4,28 @@
 
 template<typename T, int size>
 class TPQueue {
- private:
-  int begin;
-  int end;
-  T arr[5];
+private:
+int begin;
+int end;
+T arr[5];
 
- public:
-  TPQueue() : begin(0), end(0) {}
-  void push(T element) {
-    int i = end;
-    for (i; i >= begin; i--) {
-      if (element.prior > arr[i % size].prior) {
-        arr[i % size] = arr[(i - 1) % size];
-      } else {
-        break;
-      }
-    }
-    arr[i + 1] = element;
-    end++;
-  }
-  T& pop() {
-    return arr[(begin++) % size];
-  }
+public:
+TPQueue() : begin(0), end(0) {}
+void push(T element) {
+int i = end;
+for (i; i >= begin; i--) {
+if (element.prior > arr[i % size].prior) {
+arr[i % size] = arr[(i - 1) % size];
+} else {
+break;
+}
+}
+arr[i + 1] = element;
+end++;
+}
+T& pop() {
+return arr[(begin++) % size];
+}
 };
 
 struct SYM {
