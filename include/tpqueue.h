c8439a95f3030271e516ea15741c85b9d2215613
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-04-14 21:29:50 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -10,28 +10,27 @@ int end;
 T arr[5];
 
  public:
-TPQueue() {
-begin = 0;
-end = 0;
-for (int i = 0; i < 5; i++)
-arr[i].prior = 0;
-}
-void push(T element) {
-int i = end;
-for (i; i >= begin; i--) {
-if (element.prior > arr[i % size].prior) {
-arr[i % size] = arr[(i - 1) % size];
-} else {
-break;
-}
-}
-arr[(i + 1) % size] = element;
-end++;
-}
-T& pop() {
-arr[(begin) % size].prior = 0;
-return arr[(begin++) % size];
-}
+  TPQueue() {
+    begin = 0;
+    end = -1;
+    for (int i = 0; i < 5; i++)
+      arr[i].prior = 0;
+  }
+  void push(T element) {
+    int i = end;
+    for (i; i >= begin; i--) {
+      if (element.prior > arr[i % size].prior)
+        arr[(i + 1) % size] = arr[i % size];
+      else
+        break;
+    }
+    arr[(i + 1) % size] = element;
+    end++;
+  }
+  T& pop() {
+    arr[(begin) % size].prior = 0;
+    return arr[(begin++) % size];
+  }
 };
 
 struct SYM {
